c0a8f6c7108e45e309ca668331d638b18e3e2cd7
Milosz <milwrona@gmail.com>
2025-05-14 11:31:14 +0200

    wordpress integration with admin.

--- lib/wp.ts
+++ lib/wp.ts
@@ -1,17 +1,15 @@
 import { request, gql } from 'graphql-request';
 
-const endpoint = 'http://filmitnow.pl/graphql';
-
+const endpoint = 'http://admin.filmitnow.pl/graphql';
 
 export async function getHomepage() {
   const query = gql`
     query {
       page(id: "6", idType: DATABASE_ID) {
-        title
         acf {
-         heroTitle
-          heroSubtitle
           heroVideoUrl
+          portfolioVideos
+          reelsVideos
         }
       }
     }
@@ -19,11 +17,25 @@ export async function getHomepage() {
 
   try {
     const data = await request(endpoint, query);
-    return data.page.acf;
+    const homepage = data.page.acf;
+
+    const portfolioVideos = (homepage.portfolioVideos ?? "")
+      .split("\n")
+      .map((url) => url.trim())
+      .filter(Boolean);
+
+    const reelsVideos = (homepage.reelsVideos ?? "")
+      .split("\n")
+      .map((url) => url.trim())
+      .filter(Boolean);
+
+    return {
+      heroVideoUrl: homepage.heroVideoUrl,
+      portfolioVideos,
+      reelsVideos
+    };
   } catch (error) {
     console.error("❌ Błąd pobierania danych z WordPressa:", error);
     return null;
   }
 }
-
-
